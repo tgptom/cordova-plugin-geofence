8f24c9e1fe23e3f7334bf10276311ef3ec751ca3
Eric Cheung <eric.cheung@togo-pac.com>
2026-08-28 18:16:45 +0800

    Initialize lastTransitionType for GeoNotifications on boot

--- src/android/BootReceiver.java
+++ src/android/BootReceiver.java
@@ -9,6 +9,13 @@ public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Logger.setLogger(new Logger(GeofencePlugin.TAG, context, false));
+        GeoNotificationStore store = new GeoNotificationStore(context);
+        for (GeoNotification geoNotification : store.getAll()) {
+            if (geoNotification != null) {
+                geoNotification.lastTransitionType = 0;
+                store.setGeoNotification(geoNotification);
+            }
+        }
         GeoNotificationManager manager = new GeoNotificationManager(context);
         manager.loadFromStorageAndInitializeGeofences();
     }
